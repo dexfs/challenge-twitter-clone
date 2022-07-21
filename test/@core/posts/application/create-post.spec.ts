1b242b94549f2f33d879edcce0e7db3d02935118
Andre Santos <afsdab@gmail.com>
2022-07-20 22:17:04 -0300

    test: create post

---
+++ test/@core/posts/application/create-post.spec.ts
@@ -0,0 +1,67 @@
+import { Sequelize } from 'sequelize-typescript';
+import { PostModel } from '#core/posts/infra/db/repository/sequelize/post-model';
+import PostRepository from '#core/posts/infra/db/repository/sequelize/post-repository';
+import { Post } from '#core/posts/domain/entities/post';
+
+describe('Post -> Application -> UseCase', () => {
+  let sequelize: Sequelize;
+  beforeAll(() => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      host: ':memory:',
+      logging: false,
+      models: [PostModel],
+    });
+  });
+
+  beforeEach(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it('it should create a post correctly', async () => {
+    const entity = new Post({ content: 'Teste', user_id: 1 });
+    const sut = new PostRepository(PostModel);
+    await sut.insert(entity);
+    const model = await PostModel.findByPk(entity.id);
+    expect(model.toJSON()).toStrictEqual(entity.toJSON());
+  });
+
+  it('it should create a repost correctly', async () => {
+    const originalPostEntity = new Post({ content: 'Teste', user_id: 1 });
+    const repostEntity = new Post({ content: 'Teste2', user_id: 1 });
+    repostEntity.repost({
+      user_id: originalPostEntity.user_id,
+      content: originalPostEntity.content,
+      is_repost: originalPostEntity.is_repost,
+      is_quote: originalPostEntity.is_quote,
+      screen_name: `user-${originalPostEntity.user_id}`,
+      id: originalPostEntity.id,
+    });
+    const sut = new PostRepository(PostModel);
+    await sut.insert(repostEntity);
+    const model = await PostModel.findByPk(repostEntity.id);
+
+    expect(model.toJSON()).toStrictEqual(repostEntity.toJSON());
+  });
+
+  it('it should create a quote post correctly', async () => {
+    const originalPostEntity = new Post({ content: 'Teste', user_id: 1 });
+    const repostEntity = new Post({ content: 'Teste2', user_id: 1 });
+    repostEntity.quotePost('quote test', {
+      user_id: originalPostEntity.user_id,
+      content: originalPostEntity.content,
+      is_repost: originalPostEntity.is_repost,
+      is_quote: originalPostEntity.is_quote,
+      screen_name: `user-${originalPostEntity.user_id}`,
+      id: originalPostEntity.id,
+    });
+    const sut = new PostRepository(PostModel);
+    await sut.insert(repostEntity);
+    const model = await PostModel.findByPk(repostEntity.id);
+    expect(model.toJSON()).toStrictEqual(repostEntity.toJSON());
+  });
+});
--- test/@core/posts/domain/entities/post.spec.ts
+++ test/@core/posts/domain/entities/post.spec.ts
@@ -1,11 +1,11 @@
-import { Post } from '#core/posts/domain/entities/post';
+import { User } from '#core/posts/domain/entities/user';
 import * as chance from 'chance';
 import 'jest-extended';
 import { PostDataBuilder } from '../../../../fixtures/builders/post-data-builder';
 import DomainError from '#core/@shared/errors/domain-error';
 import UniqueEntityId from '#core/@shared/domain/value-objects/unique-entity-id.vo';
 
-const getOriginalPostParam = (post: Post) => ({
+const getOriginalPostParam = (post: User) => ({
   id: new UniqueEntityId(post.id),
   content: post.content,
   user_id: post.user_id,
@@ -21,45 +21,46 @@ describe('@core -> domain -> ', () => {
   });
   describe('Post', () => {
     it('create a valid entity with sent values and default', () => {
-      const post = new Post({
+      const post = new User({
         content: chanceInstance.paragraph({ sentences: 1 }),
         user_id: chanceInstance.integer(),
       });
+
       expect(post.id).not.toBeEmpty();
       expect(post.content).not.toBeEmpty();
       expect(post.content).not.toBeEmpty();
       expect(post.user_id).toBeNumber();
       expect(post.created_at).not.toBeEmpty();
       expect(post.created_at).toBeInstanceOf(Date);
-      expect(post.original_post_id).toBeUndefined();
-      expect(post.original_post_content).toBeUndefined();
-      expect(post.original_post_user_id).toBeUndefined();
-      expect(post.original_post_screen_name).toBeUndefined();
+      expect(post.original_post_id).toBeNull();
+      expect(post.original_post_content).toBeNull();
+      expect(post.original_post_user_id).toBeNull();
+      expect(post.original_post_screen_name).toBeNull();
     });
 
     it('should not allow create a repost of a repost post', () => {
-      const originalPost = new Post(
+      const originalPost = new User(
         PostDataBuilder.aPost().withRepost().build(),
       );
-      const sut = new Post(PostDataBuilder.aPost().build());
+      const sut = new User(PostDataBuilder.aPost().build());
       expect(() => sut.repost(getOriginalPostParam(originalPost))).toThrow(
         DomainError,
       );
     });
 
     it('should not allow create a quote post of a quote post', () => {
-      const originalPost = new Post(
+      const originalPost = new User(
         PostDataBuilder.aPost().withQuotePost('teste').build(),
       );
-      const sut = new Post(PostDataBuilder.aPost().build());
+      const sut = new User(PostDataBuilder.aPost().build());
       expect(() =>
         sut.quotePost('teste', getOriginalPostParam(originalPost)),
       ).toThrow(DomainError);
     });
 
     it('should create a repost correctly', () => {
-      const originalPost = new Post(PostDataBuilder.aPost().build());
-      const sut = new Post(PostDataBuilder.aPost().build());
+      const originalPost = new User(PostDataBuilder.aPost().build());
+      const sut = new User(PostDataBuilder.aPost().build());
       sut.repost(getOriginalPostParam(originalPost));
       expect(sut.original_post_id).toBe(originalPost.id);
       expect(sut.original_post_content).toBe(originalPost.content);
@@ -69,8 +70,8 @@ describe('@core -> domain -> ', () => {
     });
 
     it('should create a quote post correctly', () => {
-      const originalPost = new Post(PostDataBuilder.aPost().build());
-      const sut = new Post(PostDataBuilder.aPost().build());
+      const originalPost = new User(PostDataBuilder.aPost().build());
+      const sut = new User(PostDataBuilder.aPost().build());
       sut.quotePost('test', getOriginalPostParam(originalPost));
       expect(sut.original_post_id).toBe(originalPost.id);
       expect(sut.original_post_content).toBe(originalPost.content);
