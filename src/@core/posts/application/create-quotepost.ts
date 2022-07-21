f639ebd3fb1251a80f225dfe1e140dd6da1c446f
Andre Santos <afsdab@gmail.com>
2022-07-21 02:26:18 -0300

    feat(posts): create quotepost use case

---
+++ src/@core/posts/application/create-quotepost.ts
@@ -0,0 +1,61 @@
+import PostRepository from '#core/posts/domain/repositories/post.repository';
+import { Post } from '#core/posts/domain/entities/post';
+import UseCaseInterface from '#core/@shared/application/use-case';
+import DomainError from '#core/@shared/errors/domain-error';
+import UserRepository from '#core/users/domain/repositories/user.repository';
+import { User } from '#core/users/domain/entities/user';
+
+namespace CreateQuotePost {
+  export class UseCase implements UseCaseInterface<Input, Output> {
+    constructor(
+      private postRepository: PostRepository.Repository<Post>,
+      private userRepository: UserRepository.Repository<User>,
+    ) {}
+
+    async execute(
+      input: CreateQuotePost.Input,
+    ): Promise<CreateQuotePost.Output> {
+      const post = await this.postRepository.findById(input.post_id);
+      const user = await this.userRepository.findById(input.user_id);
+
+      if (!post) throw new DomainError('Post not found!');
+      if (!user) throw new DomainError('User not found!');
+
+      const quotePost = new Post({
+        user_id: input.user_id,
+      });
+
+      quotePost.quotePost(input.quote, {
+        id: post.id,
+        user_id: post.user_id,
+        content: post.content,
+        is_repost: post.is_repost,
+        is_quote: post.is_quote,
+        screen_name: user.username,
+      });
+      await this.postRepository.insert(quotePost);
+      return quotePost.toJSON();
+    }
+  }
+
+  export type Input = {
+    quote: string;
+    post_id: string;
+    user_id: string;
+  };
+
+  export type Output = {
+    id: string;
+    content: string | null;
+    user_id: string;
+    is_quote: boolean;
+    is_repost: boolean;
+    created_at: Date;
+    original_post_id: string | null;
+    original_post_content: string | null;
+    original_post_user_id: string | null;
+    original_post_screen_name?: string | null;
+  };
+}
+
+export default CreateQuotePost;
