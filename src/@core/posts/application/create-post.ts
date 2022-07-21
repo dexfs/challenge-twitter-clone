4ef9ef4af9f06ff11612981e8a1261d3d3f983d9
Andre Santos <afsdab@gmail.com>
2022-07-21 02:25:42 -0300

    feat(posts): create post use case

--- src/@core/posts/application/create-post.ts
+++ src/@core/posts/application/create-post.ts
@@ -20,18 +20,18 @@ export class CreatePost implements UseCase<Input, Output> {
 
 export type Input = {
   content: string;
-  user_id: number;
+  user_id: string;
 };
 
 export type Output = {
   id: string;
   content: string | null;
-  user_id: number;
+  user_id: string;
   is_quote: boolean;
   is_repost: boolean;
   created_at: Date;
   original_post_id: string | null;
   original_post_content: string | null;
-  original_post_user_id: number | null;
+  original_post_user_id: string | null;
   original_post_screen_name?: string | null;
 };
