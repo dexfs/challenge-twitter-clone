0a50a07d65d4d161793a03e66376a1788b160421
Andre Santos <afsdab@gmail.com>
2022-07-21 02:26:07 -0300

    test(posts): create repost use case

---
+++ test/@core/posts/application/create-repost.spec.ts
@@ -0,0 +1,88 @@
+import { Sequelize } from 'sequelize-typescript';
+import { PostModel } from '#core/posts/infra/db/repository/sequelize/post-model';
+import { Post } from '#core/posts/domain/entities/post';
+import PostSequelizeRepository from '#core/posts/infra/db/repository/sequelize/post-repository';
+import PostRepository from '#core/posts/domain/repositories/post.repository';
+import UserRepository from '#core/users/domain/repositories/user.repository';
+import { User } from '#core/users/domain/entities/user';
+import UserSequelizeRepository from '#core/users/infra/db/repository/sequelize/user-repository';
+import { UserModel } from '#core/users/infra/db/repository/sequelize/user-model';
+import CreateRepost from '#core/posts/application/create-repost';
+import UniqueEntityId from '#core/@shared/domain/value-objects/unique-entity-id.vo';
+
+describe('Post -> Application -> CreateRepost UseCase', () => {
+  let sequelize: Sequelize;
+  let postRepository: PostRepository.Repository<Post>;
+  let userRepository: UserRepository.Repository<User>;
+
+  beforeAll(() => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      host: ':memory:',
+      logging: false,
+      models: [PostModel, UserModel],
+    });
+  });
+
+  beforeEach(async () => {
+    postRepository = new PostSequelizeRepository(PostModel);
+    userRepository = new UserSequelizeRepository(UserModel);
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  it('it should throw an exception when nonexistent post_id sent', async () => {
+    const missingPostId = new UniqueEntityId();
+    const user = new User({ username: 'test' });
+
+    await UserModel.create(user.toJSON());
+
+    const sut = new CreateRepost.UseCase(postRepository, userRepository);
+    const promise = sut.execute({
+      user_id: user.id,
+      post_id: missingPostId.value,
+    });
+
+    await expect(promise).rejects.toThrow();
+  });
+
+  it('it should throw an exception when nonexistent user sent', async () => {
+    const missingUserId = new UniqueEntityId();
+    const post = new Post({
+      content: 'test_1',
+      user_id: missingUserId.value,
+    });
+
+    await PostModel.create(post.toJSON());
+
+    const sut = new CreateRepost.UseCase(postRepository, userRepository);
+    const promise = sut.execute({
+      user_id: missingUserId.value,
+      post_id: post.id,
+    });
+    await expect(promise).rejects.toThrow();
+  });
+
+  it('it should create a post correctly', async () => {
+    const user = new User({ username: 'test' });
+    const post = new Post({
+      content: 'test_1',
+      user_id: user.id,
+    });
+
+    await UserModel.create(user.toJSON());
+    await PostModel.create(post.toJSON());
+
+    const sut = new CreateRepost.UseCase(postRepository, userRepository);
+    const postCreated = await sut.execute({
+      user_id: user.id,
+      post_id: post.id,
+    });
+    const model = await PostModel.findByPk(postCreated.id);
+
+    expect(model.toJSON()).toStrictEqual(postCreated);
+  });
+});
