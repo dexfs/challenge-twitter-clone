55cdfa52ceb1f0f65f430703794d27ceb4a86496
Andre Santos <afsdab@gmail.com>
2022-07-21 02:46:06 -0300

    refactor(post): create post
    Added namespace to follow strucuture

--- src/@core/posts/application/create-post.ts
+++ src/@core/posts/application/create-post.ts
@@ -1,37 +1,40 @@
-import UseCase from '#core/@shared/application/use-case';
+import UseCaseInterface from '#core/@shared/application/use-case';
 import PostRepository from '#core/posts/domain/repositories/post.repository';
 import { Post } from '#core/posts/domain/entities/post';
 
-export class CreatePost implements UseCase<Input, Output> {
-  constructor(private postRepository: PostRepository.Repository<Post>) {}
+namespace CreatePost {
+  export class UseCase implements UseCaseInterface<Input, Output> {
+    constructor(private postRepository: PostRepository.Repository<Post>) {}
 
-  async execute(input: Input): Promise<Output> {
-    const entity = new Post({
-      content: input.content,
-      user_id: input.user_id,
-    });
-    await this.postRepository.insert(entity);
-    return {
-      ...entity.toJSON(),
-      original_post_id: entity.original_post_id,
-    };
+    async execute(input: Input): Promise<Output> {
+      const entity = new Post({
+        content: input.content,
+        user_id: input.user_id,
+      });
+      await this.postRepository.insert(entity);
+      return {
+        ...entity.toJSON(),
+        original_post_id: entity.original_post_id,
+      };
+    }
   }
+
+  export type Input = {
+    content: string;
+    user_id: string;
+  };
+
+  export type Output = {
+    id: string;
+    content: string | null;
+    user_id: string;
+    is_quote: boolean;
+    is_repost: boolean;
+    created_at: Date;
+    original_post_id: string | null;
+    original_post_content: string | null;
+    original_post_user_id: string | null;
+    original_post_screen_name?: string | null;
+  };
 }
-
-export type Input = {
-  content: string;
-  user_id: string;
-};
-
-export type Output = {
-  id: string;
-  content: string | null;
-  user_id: string;
-  is_quote: boolean;
-  is_repost: boolean;
-  created_at: Date;
-  original_post_id: string | null;
-  original_post_content: string | null;
-  original_post_user_id: string | null;
-  original_post_screen_name?: string | null;
-};
+export default CreatePost;
--- test/@core/posts/application/create-post.spec.ts
+++ test/@core/posts/application/create-post.spec.ts
@@ -3,7 +3,7 @@ import { PostModel } from '#core/posts/infra/db/repository/sequelize/post-model'
 import { Post } from '#core/posts/domain/entities/post';
 import PostSequelizeRepository from '#core/posts/infra/db/repository/sequelize/post-repository';
 import PostRepository from '#core/posts/domain/repositories/post.repository';
-import { CreatePost } from '#core/posts/application/create-post';
+import CreatePost from '#core/posts/application/create-post';
 import UniqueEntityId from '#core/@shared/domain/value-objects/unique-entity-id.vo';
 
 describe('Post -> Application -> UseCase', () => {
@@ -29,7 +29,7 @@ describe('Post -> Application -> UseCase', () => {
 
   it('it should create a post correctly', async () => {
     const userId = new UniqueEntityId();
-    const sut = new CreatePost(repository);
+    const sut = new CreatePost.UseCase(repository);
     const postCreated = await sut.execute({
       content: 'Teste',
       user_id: userId.value,
@@ -37,39 +37,4 @@ describe('Post -> Application -> UseCase', () => {
     const model = await PostModel.findByPk(postCreated.id);
     expect(model.toJSON()).toStrictEqual(postCreated);
   });
-
-  // it('it should create a repost correctly', async () => {
-  //   const originalPostEntity = new Post({ content: 'Teste', user_id: 1 });
-  //   const repostEntity = new Post({ content: 'Teste2', user_id: 1 });
-  //   repostEntity.repost({
-  //     user_id: originalPostEntity.user_id,
-  //     content: originalPostEntity.content,
-  //     is_repost: originalPostEntity.is_repost,
-  //     is_quote: originalPostEntity.is_quote,
-  //     screen_name: `user-${originalPostEntity.user_id}`,
-  //     id: originalPostEntity.id,
-  //   });
-  //   const sut = new PostRepository(PostModel);
-  //   await sut.insert(repostEntity);
-  //   const model = await PostModel.findByPk(repostEntity.id);
-  //
-  //   expect(model.toJSON()).toStrictEqual(repostEntity.toJSON());
-  // });
-  //
-  // it('it should create a quote post correctly', async () => {
-  //   const originalPostEntity = new Post({ content: 'Teste', user_id: 1 });
-  //   const repostEntity = new Post({ content: 'Teste2', user_id: 1 });
-  //   repostEntity.quotePost('quote test', {
-  //     user_id: originalPostEntity.user_id,
-  //     content: originalPostEntity.content,
-  //     is_repost: originalPostEntity.is_repost,
-  //     is_quote: originalPostEntity.is_quote,
-  //     screen_name: `user-${originalPostEntity.user_id}`,
-  //     id: originalPostEntity.id,
-  //   });
-  //   const sut = new PostRepository(PostModel);
-  //   await sut.insert(repostEntity);
-  //   const model = await PostModel.findByPk(repostEntity.id);
-  //   expect(model.toJSON()).toStrictEqual(repostEntity.toJSON());
-  // });
 });
