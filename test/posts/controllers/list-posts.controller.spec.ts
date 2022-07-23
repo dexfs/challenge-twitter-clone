10cec568fa3dfe7dde5ea130d8f44a3533f173a7
Andre Santos <afsdab@gmail.com>
2022-07-23 15:08:01 -0300

    test: list controller unit test

---
+++ test/posts/controllers/list-posts.controller.spec.ts
@@ -0,0 +1,20 @@
+import { ListController } from '../../../src/posts/list-posts.controller';
+
+describe('ListPostsController Unit test', () => {
+  let controller: ListController;
+
+  beforeEach(async () => {
+    controller = new ListController();
+  });
+
+  it('list posts', async () => {
+    const mockListPostUseCase = {
+      execute: jest.fn().mockReturnValue(null),
+    };
+
+    controller['listPostUseCase'] = mockListPostUseCase;
+    await controller.get({});
+    expect(controller).toBeDefined();
+    expect(mockListPostUseCase.execute).toBeCalledTimes(1);
+  });
+});
